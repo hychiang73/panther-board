b78129f8b8ec3079360b0cf70fe6be5cd08a0fb8
Dicky Chiang <dickychiang73@gmail.com>
2011-10-27 17:08:56 +0800

    update script for change to interface

--- script/ap_start.sh
+++ script/ap_start.sh
@@ -1,15 +1,26 @@
 #!/system/bin/sh
 echo "insmod driver"
 insmod /system/lib/modules/wl12xx_sdio.ko
+
 echo "set country code from .config"
 iw reg set `busybox grep country_code= /data/misc/wifi/hostapd.conf | busybox sed "s:country_code=::"`
+
+echo "delet wlan0"
+iw wlan0 del
+
+echo "get phy to interfer wlan1"
+iw `ls /sys/class/ieee80211/` interface add wlan1 type managed
+
 echo "get register of hw"
 iw reg get
+
 echo "start hostapd service"
 setprop ctl.start hostapd_bin
 sleep 3
-echo "start interface of wlan0"
+
+echo "start interface wlan1"
 setprop ctl.start ifcfg_softap
 sleep 3
+
 echo "start dhcp service daemon"
 setprop ctl.start dhcpd_softap
--- script/ap_stop.sh
+++ script/ap_stop.sh
@@ -1,11 +1,16 @@
 #!/system/bin/sh
 echo "stop dhcp service"
 setprop ctl.stop dhcpd_softap
-echo "wlan0 to down"
-ifconfig wlan0 down
+
 sleep 3
+
+echo "wlan1 to down"
+ifconfig wlan1 down
+
+sleep 3
+
 echo "stop hostap service"
 setprop ctl.stop hostapd_bin
-sleep 3
+
 #echo "rmmod driver"
 #rmmod wl12xx_sdio
